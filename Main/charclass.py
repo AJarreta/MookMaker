63c5368b39f1e9a55a126114f8b7961c4fe614a5
Antonio Jarreta <antoniojarreta@gmail.com>
2016-08-25 14:14:58 +0100

    Added A-M civilian skills

--- Main/charclass.py
+++ Main/charclass.py
@@ -4,7 +4,13 @@ class CthulhuCharacter(object):
                  "INT": 0, "POW": 0, "DEX": 0, \
                  "APP": 0, "EDU": 0, "SAN": 0}
     CharDerivedStats = {"Dam. Bonus": 0, "HP": 0, "Idea": 0, "Luck": 0, "MP": 0, "Knowledge": 0}
-    CharSkills = {}
+    CharSkills = {"Accounting": 10, "Anthropology": 1, "Archaeology": 1, "Art": 5, "Astronomy": 1, \
+                  "Bargain": 5, "Biology": 1, "Chemistry": 1, "Climb": 40, "Conceal": 15, "Craft": 5,\
+                  "Credit Rating": 15, "Cthulhu Mythos": 0, "Disguise": 1, "Dodge": 0, "Drive Auto": 20,\
+                  "Electric Repair": 10, "Fast Talk": 5, "First Aid": 30, "Geology": 1, "Hide": 10, \
+                  "History": 20, "Jump" 25, "Law": 5, "Library Use": 25, "Listen": 25, "Locksmith" : 1, \
+                  "Martial Arts": 1, }
+    CharCombatSkills = {}
 
     def __init__(self):
         return super(CthulhuCharacter, self).__init__(*args, **kwargs)
