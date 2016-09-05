c2b98a2221ee91e60e8e51f45f9d9c79d20de4cf
Antonio Jarreta <antoniojarreta@gmail.com>
2016-09-05 15:03:39 +0100

    Finished main class, added profession children classes

--- DiceThrower/DiceThrower.py
+++ DiceThrower/DiceThrower.py
@@ -17,7 +17,7 @@ window = Tkinter.Tk()
 window.title("Dice Thrower")
 
 mainframe = ttk.Frame(window, padding='3 3 12 12')
-mainframe.grid(column=0, row=0, sticky=NSEW)
+mainframe.grid(column=0, row=0)
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 
@@ -26,15 +26,15 @@ NumberOfSides = Tkinter.IntVar()
 ThrowResult = Tkinter.IntVar()
 
 DiceInput = ttk.Entry(window, width=4, textvariable=NumberOfDice)
-DiceInput.grid(column=1, row=1, sticky=E)
+DiceInput.grid(column=1, row=1)
 
-ttk.Label(window, textvariable="d").grid(column=2, row=2, sticky=(W, E))
+ttk.Label(window, textvariable='d').grid(column=2, row=2)
 
 SideInput = ttk.Entry(window, width=4, textvariable=NumberOfSides)
-SideInput.grid(column=3, row=1, sticky=(W, E))
+SideInput.grid(column=3, row=1)
 
-ttk.Button(window, text="Throw!", command=DiceThrow).grid(column=4, row=1, sticky=W)
-ttk.Label(window, textvariable="The result of the throw is").grid(column=1, row=2, sticky=W)
-ttk.Label(window, textvariable=ThrowResult).grid(column=3, row=2, sticky=W)
+ttk.Button(window, text="Throw!", command=DiceThrow).grid(column=4, row=1)
+ttk.Label(window, textvariable="The result of the throw is").grid(column=1, row=2)
+ttk.Label(window, textvariable=ThrowResult).grid(column=3, row=2)
 
 window.mainloop()
--- MookMaker/charclass.py
+++ MookMaker/charclass.py
@@ -8,11 +8,26 @@ class CthulhuCharacter(object):
                   "Bargain": 5, "Biology": 1, "Chemistry": 1, "Climb": 40, "Conceal": 15, "Craft": 5,\
                   "Credit Rating": 15, "Cthulhu Mythos": 0, "Disguise": 1, "Dodge": 0, "Drive Auto": 20,\
                   "Electric Repair": 10, "Fast Talk": 5, "First Aid": 30, "Geology": 1, "Hide": 10, \
-                  "History": 20, "Jump" 25, "Law": 5, "Library Use": 25, "Listen": 25, "Locksmith" : 1, \
-                  "Martial Arts": 1, }
-    CharCombatSkills = {}
+                  "History": 20, "Jump": 25, "Law": 5, "Library Use": 25, "Listen": 25, "Locksmith" : 1, \
+                  "Martial Arts": 1, "Mechanical Repair": 20, "Medicine": 5, "Natural History": 10, \
+                  "Navigate": 10, "Occult": 5, "Operate Heavy Machinery": 1, "Other Language": 1, \
+                  "Own Language": 0, "Persuade": 15, "Pharmacy": 1, "Photography": 10, "Physics": 1, \
+                  "Pilot": 1, "Psychoanalysis": 1, "Psychology": 5, "Ride": 5, "Sneak": 10, "Spot Hidden": 25, \
+                  "Swim": 25, "Throw": 25, "Track": 10}
+    CharCombatSkills = {"Fist": 50, "Grapple": 25, "Headbutt": 10, "Kick": 25, "Axe": 20, "Club": 25, "Dagger": 25, \
+                        "Knife": 25, "Rapier": 20, "Sabre": 15, "Sword": 10, "Handgun": 20, "Machine Gun": 15, \
+                        "Rifle": 25, "Shotgun": 30, "SMG": 15}
+    ClassSkillPoints = 0
+    FreeSkillPoints = 0
 
     def __init__(self):
+        self.CharPersonal = CharPersonal
+        self.CharStats = CharStats
+        self.CharDerivedStats = CharDerivedStats
+        self.CharSkills = CharSkills
+        self.CharCombatSkills = CharCombatSkills
+        self.ClassSkillPoints = ClassSkillPoints
+        self.FreeSkillPoints = FreeSkillPoints
         return super(CthulhuCharacter, self).__init__(*args, **kwargs)
 
     def EDURoll(self):
@@ -23,7 +38,7 @@ class CthulhuCharacter(object):
         result += 3
         return result
 
-    def PercentRoll(self)_
+    def PercentRoll(self):
         result = random.randint(1, 100) 
         return result
         
@@ -59,3 +74,39 @@ class CthulhuCharacter(object):
                           "INT": 0, "POW": 0, "DEX": 0, \
                           "APP": 0, "EDU": 0, "SAN": 0}
         self.CharDerivedStats = {"Dam. Bonus": 0, "HP": 0, "Idea": 0, "Luck": 0, "MP": 0, "Knowledge": 0}
+
+class Lawyer(CthulhuCharacter):
+
+class Antiquarian(CthulhuCharacter):
+
+class Parapsychologist(CthulhuCharacter):
+
+class Writer(CthulhuCharacter):
+
+class PrivateInvestigator(CthulhuCharacter):
+
+class Journalist(CthulhuCharacter:
+
+class Dilettante(CthulhuCharacter):
+
+class Doctor(CthulhuCharacter):
+
+class CollegeProfessor(CthulhuCharacter):
+
+class Revolutionary(CthulhuCharacter):
+
+class Farmer(CthulhuCharacter):
+
+class Politician(CthulhuCharacter):
+
+class Athlete(CthulhuCharacter):
+
+class Missionary(CthulhuCharacter):
+
+class Soldier(CthulhuCharacter):
+
+class Gangster(CthulhuCharacter):
+
+class Police(CthulhuCharacter):
+
+class Hobo(CthulhuCharacter):
