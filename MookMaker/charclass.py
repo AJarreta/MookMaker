f492c488e1e5f69ed9dd488ba7881b48bfd32582
Antonio Jarreta <antoniojarreta@gmail.com>
2016-09-06 14:40:04 +0100

    Added StatsGeneration function for all classes

--- MookMaker/charclass.py
+++ MookMaker/charclass.py
@@ -21,13 +21,6 @@ class CthulhuCharacter(object):
     FreeSkillPoints = 0
 
     def __init__(self):
-        self.CharPersonal = CharPersonal
-        self.CharStats = CharStats
-        self.CharDerivedStats = CharDerivedStats
-        self.CharSkills = CharSkills
-        self.CharCombatSkills = CharCombatSkills
-        self.ClassSkillPoints = ClassSkillPoints
-        self.FreeSkillPoints = FreeSkillPoints
         return super(CthulhuCharacter, self).__init__(*args, **kwargs)
 
     def EDURoll(self):
@@ -76,37 +69,235 @@ class CthulhuCharacter(object):
         self.CharDerivedStats = {"Dam. Bonus": 0, "HP": 0, "Idea": 0, "Luck": 0, "MP": 0, "Knowledge": 0}
 
 class Lawyer(CthulhuCharacter):
+    
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Lawyer, self).StatRollNormal()
+        self.CharStats["CON"] = super(Lawyer, self).StatRollNormal()
+        self.CharStats["SIZ"] = super(Lawyer, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Lawyer, self).StatRollXHigh()
+        self.CharStats["POW"] = super(Lawyer, self).StatRollHigh
+        self.CharStats["DEX"] = super(Lawyer, self).StatRollNormal
+        self.CharStats["APP"] = super(Lawyer, self).StatRollNormal
+        self.CharStats["EDU"] = super(Lawyer, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Antiquarian(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Antiquarian, self).StatRollNormal
+        self.CharStats["CON"] = super(Antiquarian, self).StatRollNormal
+        self.CharStats["SIZ"] = super(Antiquarian, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Antiquarian, self).StatRollHigh
+        self.CharStats["POW"] = super(Antiquarian, self).StatRollXHigh
+        self.CharStats["DEX"] = super(Antiquarian, self).StatRollNormal
+        self.CharStats["APP"] = super(Antiquarian, self).StatRollNormal
+        self.CharStats["EDU"] = super(Antiquarian, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class Parapsychologist(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Parapsychologist, self).StatRollNormal
+        self.CharStats["CON"] = super(Parapsychologist, self).StatRollNormal
+        self.CharStats["SIZ"] = super(Parapsychologist, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Parapsychologist, self).StatRollNormal
+        self.CharStats["POW"] = super(Parapsychologist, self).StatRollXHigh
+        self.CharStats["DEX"] = super(Parapsychologist, self).StatRollNormal
+        self.CharStats["APP"] = super(Parapsychologist, self).StatRollHigh
+        self.CharStats["EDU"] = super(Parapsychologist, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Writer(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Writer, self).
+        self.CharStats["CON"] = super(Writer, self).
+        self.CharStats["SIZ"] = super(Writer, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Writer, self).
+        self.CharStats["POW"] = super(Writer, self).
+        self.CharStats["DEX"] = super(Writer, self).
+        self.CharStats["APP"] = super(Writer, self).
+        self.CharStats["EDU"] = super(Writer, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class PrivateInvestigator(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(PrivateInvestigator, self).
+        self.CharStats["CON"] = super(PrivateInvestigator, self).
+        self.CharStats["SIZ"] = super(PrivateInvestigator, self).StatRollXHigh()
+        self.CharStats["INT"] = super(PrivateInvestigator, self).
+        self.CharStats["POW"] = super(PrivateInvestigator, self).
+        self.CharStats["DEX"] = super(PrivateInvestigator, self).
+        self.CharStats["APP"] = super(PrivateInvestigator, self).
+        self.CharStats["EDU"] = super(PrivateInvestigator, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Journalist(CthulhuCharacter:
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Journalist, self).
+        self.CharStats["CON"] = super(Journalist, self).
+        self.CharStats["SIZ"] = super(Journalist, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Journalist, self).
+        self.CharStats["POW"] = super(Journalist, self).
+        self.CharStats["DEX"] = super(Journalist, self).
+        self.CharStats["APP"] = super(Journalist, self).
+        self.CharStats["EDU"] = super(Journalist, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class Dilettante(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Dilettante, self).
+        self.CharStats["CON"] = super(Dilettante, self).
+        self.CharStats["SIZ"] = super(Dilettante, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Dilettante, self).
+        self.CharStats["POW"] = super(Dilettante, self).
+        self.CharStats["DEX"] = super(Dilettante, self).
+        self.CharStats["APP"] = super(Dilettante, self).
+        self.CharStats["EDU"] = super(Dilettante, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Doctor(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Doctor, self).
+        self.CharStats["CON"] = super(Doctor, self).
+        self.CharStats["SIZ"] = super(Doctor, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Doctor, self).
+        self.CharStats["POW"] = super(Doctor, self).
+        self.CharStats["DEX"] = super(Doctor, self).
+        self.CharStats["APP"] = super(Doctor, self).
+        self.CharStats["EDU"] = super(Doctor, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class CollegeProfessor(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(CollegeProfessor, self).
+        self.CharStats["CON"] = super(CollegeProfessor, self).
+        self.CharStats["SIZ"] = super(CollegeProfessor, self).StatRollXHigh()
+        self.CharStats["INT"] = super(CollegeProfessor, self).
+        self.CharStats["POW"] = super(CollegeProfessor, self).
+        self.CharStats["DEX"] = super(CollegeProfessor, self).
+        self.CharStats["APP"] = super(CollegeProfessor, self).
+        self.CharStats["EDU"] = super(CollegeProfessor, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Revolutionary(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Revolutionary, self).
+        self.CharStats["CON"] = super(Revolutionary, self).
+        self.CharStats["SIZ"] = super(Revolutionary, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Revolutionary, self).
+        self.CharStats["POW"] = super(Revolutionary, self).
+        self.CharStats["DEX"] = super(Revolutionary, self).
+        self.CharStats["APP"] = super(Revolutionary, self).
+        self.CharStats["EDU"] = super(Revolutionary, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class Farmer(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Farmer, self).
+        self.CharStats["CON"] = super(Farmer, self).
+        self.CharStats["SIZ"] = super(Farmer, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Farmer, self).
+        self.CharStats["POW"] = super(Farmer, self).
+        self.CharStats["DEX"] = super(Farmer, self).
+        self.CharStats["APP"] = super(Farmer, self).
+        self.CharStats["EDU"] = super(Farmer, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Politician(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Politician, self).
+        self.CharStats["CON"] = super(Politician, self).
+        self.CharStats["SIZ"] = super(Politician, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Politician, self).
+        self.CharStats["POW"] = super(Politician, self).
+        self.CharStats["DEX"] = super(Politician, self).
+        self.CharStats["APP"] = super(Politician, self).
+        self.CharStats["EDU"] = super(Politician, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class Athlete(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Athlete, self).
+        self.CharStats["CON"] = super(Athlete, self).
+        self.CharStats["SIZ"] = super(Athlete, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Athlete, self).
+        self.CharStats["POW"] = super(Athlete, self).
+        self.CharStats["DEX"] = super(Athlete, self).
+        self.CharStats["APP"] = super(Athlete, self).
+        self.CharStats["EDU"] = super(Athlete, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Missionary(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Missionary, self).
+        self.CharStats["CON"] = super(Missionary, self).
+        self.CharStats["SIZ"] = super(Missionary, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Missionary, self).
+        self.CharStats["POW"] = super(Missionary, self).
+        self.CharStats["DEX"] = super(Missionary, self).
+        self.CharStats["APP"] = super(Missionary, self).
+        self.CharStats["EDU"] = super(Missionary, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class Soldier(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Soldier, self).
+        self.CharStats["CON"] = super(Soldier, self).
+        self.CharStats["SIZ"] = super(Soldier, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Soldier, self).
+        self.CharStats["POW"] = super(Soldier, self).
+        self.CharStats["DEX"] = super(Soldier, self).
+        self.CharStats["APP"] = super(Soldier, self).
+        self.CharStats["EDU"] = super(Soldier, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
 
 class Gangster(CthulhuCharacter):
 
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Gangster. self).
+        self.CharStats["CON"] = super(Gangster. self).
+        self.CharStats["SIZ"] = super(Gangster, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Gangster. self).
+        self.CharStats["POW"] = super(Gangster. self).
+        self.CharStats["DEX"] = super(Gangster. self).
+        self.CharStats["APP"] = super(Gangster. self).
+        self.CharStats["EDU"] = super(Gangster, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
 class Police(CthulhuCharacter):
 
-class Hobo(CthulhuCharacter):
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Police, self).
+        self.CharStats["CON"] = super(Police, self).
+        self.CharStats["SIZ"] = super(Police, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Police, self).
+        self.CharStats["POW"] = super(Police, self).
+        self.CharStats["DEX"] = super(Police, self).
+        self.CharStats["APP"] = super(Police, self).
+        self.CharStats["EDU"] = super(Police, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
+
+class Drifter(CthulhuCharacter):
+
+    def StatsGeneration(self):
+        self.CharStats["STR"] = super(Drifter, self).
+        self.CharStats["CON"] = super(Drifter, self).
+        self.CharStats["SIZ"] = super(Drifter, self).StatRollXHigh()
+        self.CharStats["INT"] = super(Drifter, self).
+        self.CharStats["POW"] = super(Drifter, self).
+        self.CharStats["DEX"] = super(Drifter, self).
+        self.CharStats["APP"] = super(Drifter, self).
+        self.CharStats["EDU"] = super(Drifter, self).EDURoll()
+        self.CharStats["SAN"] = self.CharStats["POW"] * 5
