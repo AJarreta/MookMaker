7b62d4dcc3de96f5e5e7aa1008c3d61cc69d4ec7
AJarreta <antoniojarreta@gmail.com>
2016-09-08 16:06:25 +0100

    Update DiceThrower.py
    Corrected text bugs in the DiceThrower

--- DiceThrower/DiceThrower.py
+++ DiceThrower/DiceThrower.py
@@ -17,7 +17,7 @@ window = Tkinter.Tk()
 window.title("Dice Thrower")
 
 mainframe = ttk.Frame(window, padding='3 3 12 12')
-mainframe.grid(column=0, row=0)
+mainframe.grid(column=0, row=0, sticky='N S W E')
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 
@@ -28,13 +28,13 @@ ThrowResult = Tkinter.IntVar()
 DiceInput = ttk.Entry(window, width=4, textvariable=NumberOfDice)
 DiceInput.grid(column=1, row=1)
 
-ttk.Label(window, textvariable='d').grid(column=2, row=2)
+ttk.Label(window, text='d').grid(column=2, row=2, sticky='W E')
 
 SideInput = ttk.Entry(window, width=4, textvariable=NumberOfSides)
 SideInput.grid(column=3, row=1)
 
-ttk.Button(window, text="Throw!", command=DiceThrow).grid(column=4, row=1)
-ttk.Label(window, textvariable="The result of the throw is").grid(column=1, row=2)
-ttk.Label(window, textvariable=ThrowResult).grid(column=3, row=2)
+ttk.Button(window, text="Throw!", command=DiceThrow).grid(column=4, row=1, sticky='W E')
+ttk.Label(window, text="The result of the throw is").grid(column=1, row=2, sticky='E')
+ttk.Label(window, textvariable=ThrowResult).grid(column=3, row=2, sticky='E')
 
 window.mainloop()
